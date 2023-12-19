1fcacc8f81173a8d7e629dbd45fb4ddffcdabb52
Amy-038 <amy.sirma@student.moringaschool.com>
2023-12-19 21:18:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (element) {
+        return element.toUpperCase() === string.toUpperCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (element) {
+        return element.slice(0,2) === string;
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function (element) {
+        return element.name === string;
+    })
+}
+
